b138bdef5e75767f52eeb7dffcaf210fa6e95bcf
Fauzi <fauzibinfaisal@gmail.com>
2019-04-25 14:17:08 +0700

    initial commit

--- BMI Calculatify/ViewController.swift
+++ BMI Calculatify/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var weightCounter: UILabel!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var heightCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK:- Manage Resource
+    @IBAction func weightChange(_ sender: UISlider) {
+        
+        let currentValue = String(format: "%.2f", Double(sender.value))
+        
+        weightCounter.text = "\(currentValue) kg"
+        
+        
+    }
+    
+    
+    @IBAction func heightChange(_ sender: UISlider) {
+    }
+    @IBAction func changeMode(_ sender: UISegmentedControl) {
+//        var mode: Mode = .light
+    }
+    
+    func updateView(){
+    let finalResult = ""
+    
+    }
 }
 
